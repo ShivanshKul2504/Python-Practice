81fdfd879e23a4c2985007f52f613106bed6fed7
Shivansh Kulshrestha <shivansh.kulshrestha@zypp.app>
2023-03-30 09:43:14 +0530

    PythonDay-1 Save

---
+++ PythonDay-1.py
@@ -0,0 +1,31 @@
+### General Introduction to Python
+
+## Some important notes about Python
+
+'''
+1. Python is a high level Programming Language
+2. Python was originally developed by Guido Van Rossum in 1989. But Python came into the real world in 1991.
+3. Its name was derived from The Monty Python Circus .
+4. Guido Van Rossum developed python by taking into account the other languages. He took the following
+    features from Different languages.
+    a ) Functional Programming Features from C programming language.
+    b ) Scripting Language Features from Perl
+    c ) Object Oriented Programming Features from C++
+    d ) Modular Programming Features from Modula-3
+
+USES OF PYTHON 
+1. In web development
+2. In AI
+3. In Machine Learning
+4. In Data Science
+5. In Data Visualization
+6. In IOT
+7. In App Developement
+
+FEATURES OF PYTHON 
+1. Simple and Easy to Learn
+2. Interpreted
+3. Both Procedure Oriented and Object Oriented
+4. Freeware and Open Source and Large Community
+5.  
+'''
