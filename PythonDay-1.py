9d2b996f1a09ee3708c5d7ef1ee033d366a34c01
Shivansh Kulshrestha <shivansh.kulshrestha@zypp.app>
2023-04-05 15:52:42 +0530

    Python Day - 1

--- PythonDay-1.py
+++ PythonDay-1.py
@@ -14,6 +14,7 @@
     d ) Modular Programming Features from Modula-3
 
 USES OF PYTHON 
+
 1. In web development
 2. In AI
 3. In Machine Learning
@@ -23,6 +24,7 @@ USES OF PYTHON
 7. In App Developement
 
 FEATURES OF PYTHON 
+
 1. Simple and Easy to Learn
 2. Interpreted
 3. Both Procedure Oriented and Object Oriented
@@ -30,6 +32,104 @@ FEATURES OF PYTHON
 5. Platform Independent 
 6. Portable
 7. Dynamically Types Programming Language
+8. Interpreted 
+9. Extensible Programming Language
+10. Embedded Programming Language
+11. Extensive Library 
+
+
+
+
+"Python is platform independent" means that Python code can run on any platform without requiring any modifications to the code.
+This is because Python code is interpreted at runtime, and the Python interpreter is available for most operating systems.
+So, as long as the appropriate version of the Python interpreter is installed on a particular platform,
+ the same Python code should run on that platform without any changes.
+
+
+ "Python has portability" means that Python code can be easily moved or copied from one platform to another.
+This is because Python code is usually stored in text files that can be easily edited and transferred between different platforms.
+  Additionally, Python libraries and modules are designed to be cross-platform,
+so they can be used on different platforms without requiring any modifications.
+
+
+
+LIMITATIONS OF PYTHON 
+
+1. Performance wise python is not up to the mark as python is a interpreted language.
+2. Python is not used to develop mobile applications.
+
+
+
+FLAVORS OF PYTHON 
+
+1. Cpython  : It is the standard flavor of python used to work with C programming language. 
+2. Jpython  : It is used to work with Java programming language.
+3. PyPy : It is faster than python as it uses the JIT compiler.
+4. ironPython : It is made to work with C# programming language.
+5. RubyPython : It is made to work with Ruby Programming language.
+6. AncondaPython : It is made to work with large volumes of data.
+
+
+Python Versions : 
+1.0 - 1994
+2.0 - 2000
+3.0 - 2008
+
+
+
+
+IDENTIFIERS 
+
+A name in python program is known as the identifier.
+It can be a class Name or Module name or function name or a variable name.
+
+RULES FOR DEFINING IDENTIFIERS IN PYTHON 
+
+1. The only allowed characters are : 
+  a ) numbers 
+  b ) _ 
+  c ) alphabets 
+
+  # Special Characters are not allowed.
+  
+2. Identifiers cannot start with a digit in python.
+
+3. Identifiers in python are case sensitive.
+
+4. We cannot use reserve words (keywords) as identifiers in python.
+
+5. There is no length limit to identifiers in python.
+
+6. Special Characters are not allowed in Identifiers.
+
+7. If an identifier starts with a '_' in python, it means it is private
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
